3445d3e0a1752fdba95f724ac4af73362c193447
umjinsun12 <umjinsun12@gmail.com>
2016-07-02 17:01:18 +0900

    .

---
+++ 1182/1182-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int a[21];
+
+int main()
+{
+	int n, s;
+	cin >> n >> s;
+
+	for(int i=0 ; i < n ; i++)
+		cin >> a[i];
+
+
+	int ans =0;
+	for(int i=1 ; i < (1<<n); i++)
+	{
+		int sum=0;
+		for(int k=0; k < n ; k++)
+		{
+			if(i&(1<<k))
+			{
+				sum += a[k];
+			}
+		}
+
+		if(sum==s)
+			ans++;
+	}
+
+	cout << ans << '\n';
+
+}
