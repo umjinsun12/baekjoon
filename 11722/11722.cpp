5318d987bdfed3a448eb90a060f33ba592c8cd8c
umjinsun12 <umjinsun12@gmail.com>
2016-07-21 11:38:47 +0900

    .

---
+++ 11722/11722.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int d[1001];
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	for (int i = 0; i < n; i++)
+	{
+		d[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[i] < a[j] && d[i] < d[j] + 1)
+				d[i] = d[j] + 1;
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (ans < d[i])
+			ans = d[i];
+	}
+
+	cout << ans << '\n';
+}
