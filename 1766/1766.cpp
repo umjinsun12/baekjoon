46b40271ad1db7a6fd354a15582ee21005c7a673
umjinsun12 <umjinsun12@gmail.com>
2016-08-22 21:23:21 +0900

    priorty_queue를 이용한 위상정렬

---
+++ 1766/1766.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector <int> a[32001];
+int ind[32001];
+
+int main()
+{
+  int n,m;
+  cin >> n >> m;
+  for(int i=0 ; i<m ; i++)
+  {
+    int x,y;
+    cin >> x >> y;
+    a[x].push_back(y);
+    ind[y]++;
+  }
+
+  priority_queue<int> q;
+  for(int i=1; i<=n ;i++)
+  {
+    if(ind[i]==0)
+      q.push(-i);
+  }
+
+  while(!q.empty())
+  {
+    int x = -q.top();
+    q.pop();
+    cout << x << " ";
+    for(int i=0; i < a[x].size(); i++)
+    {
+      int y = a[x][i];
+      ind[y]--;
+      if(ind[y]==0)
+        q.push(-y);
+    }
+  }
+
+  cout << '\n';
+
+}
